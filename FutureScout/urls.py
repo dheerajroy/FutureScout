213599920e6e3c50f314fb285a448cac4de9061b
shresthacodes <invinciblecodes@gmail.com>
2023-11-07 18:13:52 +0530

    catalog done

--- FutureScout/urls.py
+++ FutureScout/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path("__reload__/", include("django_browser_reload.urls")),
     path('admin/', admin.site.urls),
     path('', include('roadmap.urls')),
+
 ]
--- roadmap/urls.py
+++ roadmap/urls.py
@@ -1,9 +1,19 @@
 from django.urls import path
 from .views import SearchView, ResultView
-
+from . import views
 app_name = 'roadmap'
 
 urlpatterns = [
+    path('home/',views.home_view, name='home'),
     path('', SearchView.as_view(), name='search'),
     path('<slug:current_level>/', ResultView.as_view(), name='result'),
+    path('10th/', ResultView.as_view(), name='10th'),
+    path('12th/', ResultView.as_view(), name='12th'),
+    path('diploma/', ResultView.as_view(), name='diploma'),
+    path('ug/', ResultView.as_view(), name='ug'),
+    path('pg/', ResultView.as_view(), name='pg'),
+    path('phd/', ResultView.as_view(), name='phd'),
+    
+    
+    
 ]
